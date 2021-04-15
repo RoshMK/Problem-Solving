8f46aa16c1323daef3af659914be52422e6ebefd
Roshmk <roshanmk94@gmail.com>
2021-04-15 13:28:21 +0530

    Class Added

--- phone_number.rb
+++ phone_number.rb
@@ -1,19 +1,22 @@
-#checks the format of a given number
-def phon_num(num)
-    a = num
-    # converts to an array of charachters
-    num.split("")
-    if num.length == 14
-      # checking the format for correct spaces, - and ()
-      if (num[0] == "(" && num[4] == ")") && (num[5] == " "&&num[9] == "-")
-        puts "Your number #{a} is valid"
+class PhoneNumber
+  #checks the format of a given number
+  def phon_num(num)
+      a = num
+      # converts to an array of charachters
+      num.split("")
+      if num.length == 14
+        # checking the format for correct spaces, - and ()
+        if (num[0] == "(" && num[4] == ")") && (num[5] == " "&&num[9] == "-")
+          puts "Your number #{a} is valid"
+        else
+          puts "Invalid number format"
+        end
       else
         puts "Invalid number format"
       end
-    else
-      puts "Invalid number format"
-    end
   end
+end
+  ph = PhoneNumber.new
   puts "Enter your phone number to check if its in valid format"
   number = gets.chomp
-  phon_num(number) 
+  ph.phon_num(number)
