7ba1ac89e1091ac84eb9685ddc7015c57f2928d6
Roshmk <roshanmk94@gmail.com>
2021-04-16 12:32:15 +0530

    Final Phone Number file.

--- phone_number.rb
+++ phone_number.rb
@@ -2,21 +2,37 @@ class PhoneNumber
   #checks the format of a given number
   def phon_num(num)
       a = num
+      ch = 0
       # converts to an array of charachters
       num.split("")
       if num.length == 14
         # checking the format for correct spaces, - and ()
-        if (num[0] == "(" && num[4] == ")") && (num[5] == " "&&num[9] == "-")
+        for i in 0...14
+          #puts "#{num[i]} "
+          if i == 0 || i == 4 || i == 5 || i == 9
+            next
+          end
+          if num[i].match?(/\d/)
+            ch += 1
+          end
+        end
+        if (num[0] == "(" && num[4] == ")") && (num[5] == " "&&num[9] == "-") && ch == 10
           puts "Your number #{a} is valid"
+          return true
         else
-          puts "Invalid number format"
+          puts "Your number #{a} is in invalid number format"
+          return false
         end
       else
-        puts "Invalid number format"
+        puts "Your number #{a} invalid number format"
+        return false
       end
   end
+  
 end
-  ph = PhoneNumber.new
-  puts "Enter your phone number to check if its in valid format"
-  number = gets.chomp
-  ph.phon_num(number)
+=begin
+ph = PhoneNumber.new
+puts "Enter your phone number to check if its in valid format"
+number = gets.chomp
+ph.phon_num(number)
+=end
