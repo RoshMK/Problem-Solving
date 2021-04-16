17defc4c82b8f079e57e9d4acbf9b402dd8e607c
Roshmk <roshanmk94@gmail.com>
2021-04-16 13:46:16 +0530

    Final Word Sort File

--- word_sorting.rb
+++ word_sorting.rb
@@ -1,22 +1,32 @@
-puts "Enter a sentance to sort"
-strng =gets.chomp
-s_ary = strng.split(" ")
-a = s_ary.length
-for i in 0...a
-  for j in 0...a
-    if s_ary[i].length < s_ary[j].length
-      b = s_ary[i]
-      s_ary[i] = s_ary[j]
-      s_ary[j] = b
-    elsif s_ary[i].length == s_ary[j].length
-      if s_ary[i] < s_ary[j]
-        b = s_ary[i]
-        s_ary[i] = s_ary[j]
-        s_ary[j] = b
+class WordSort
+  def wsort(strng)
+    s_ary = strng.split(" ")
+    a = s_ary.length
+    for i in 0...a
+      for j in 0...a
+        if s_ary[i].length < s_ary[j].length
+          b = s_ary[i]
+          s_ary[i] = s_ary[j]
+          s_ary[j] = b
+        elsif s_ary[i].length == s_ary[j].length
+          if s_ary[i] < s_ary[j]
+            b = s_ary[i]
+            s_ary[i] = s_ary[j]
+            s_ary[j] = b
+          end
+        end
       end
     end
+    s = ""
+    s_ary.each { |n| s.concat(" #{n}") }
+    puts "#{s}"
+    return s
   end
 end
-s = ""
-s_ary.each { |n| s.concat(" #{n}") }
-puts "#{s}"
+
+=begin
+ws = WordSort.new
+puts "Enter a sentance to sort"
+st =gets.chomp
+ws.wsort(st)
+=end
