f75b66b95f0a804c5ea732353d45b2b58b55cc62
Roshmk <roshanmk94@gmail.com>
2021-04-16 12:33:02 +0530

    Phone Number Test File

--- phone_number_test.rb
+++ phone_number_test.rb
@@ -3,44 +3,39 @@ require_relative 'phone_number'
 
 class PhoneNumberTEST < Minitest::Test
 
-    def test_minitestobj
+    def setup
         @phnum = PhoneNumber.new
     end        
 
-    def test_validate_correct_format
-        assert @phnum.phon_num("(702) 555-9079")
+    def test_phonenumber
+        assert_equal true, @phnum.phon_num("(125) 456-7896")
+    end
+    
+    def test_less_number
+        assert_equal false, @phnum.phon_num("(702) 55-9079")
     end
 
-    def test_invalidate_less_digits
-        refute @phnum.phon_num("(702) 555-9079")
+    def test_not_digits
+        assert_equal false, @phnum.phon_num("(702) ab5-9079")
     end
 
-    def test_invalidate_not_digits
-        refute @phnum.phon_num("(702) 555-9079")
+    def test_more_numbers
+        assert_equal false, @phnum.phon_num("(702) 55875-9079")
     end
 
-    def test_invalidate_more_digits
-        refute @phnum.phon_num("(702) 555-9079")
+    def test_bracket_missing
+        assert_equal false, @phnum.phon_num("(702 555-9079")
     end
 
-    def test_invalidate_no_brackets
-        refute @phnum.phon_num("(702) 555-9079")
+    def test_dash_missing
+        assert_equal false, @phnum.phon_num("(702) 555 9079")
     end
 
-    def test_invalidate_no_dash
-        refute @phnum.phon_num("(702) 555-9079")
+    def test_bracket_misplaced
+        assert_equal false, @phnum.phon_num("702 (555)-9079")
     end
 
-    def invalidate_misplaced_bracket
-        refute @phnum.phon_num("(702) 555-9079")
-    end
-
-    def test_invalidate_misplaced_dash
-        refute @phnum.phon_num("(702) 555-9079")
-    end
-
-    def test_invalidate_misplaced_spaces
-        refute @phnum.phon_num("(702) 555-9079")
+    def test_dash_misplaced
+        assert_equal false, @phnum.phon_num("(702)-555 9079")
     end
 end
-
