e00001120c81c3a4dc2c3fa55bb123a0668122bf
Roshmk <roshanmk94@gmail.com>
2021-04-17 10:49:24 +0530

    Final

--- deleting_occurrences.rb
+++ deleting_occurrences.rb
@@ -1,3 +1,4 @@
+=begin
 puts "Enter an array of elements with any number of ocurences and enter '!' to end array"
 a = "#"
 arr = []
@@ -13,28 +14,36 @@ while a == "#"
  end
 end
 #occurances count
-b = arr.length
+
 puts "Enter the number of ocurences for elements in array"
 z = gets.chomp
 occ = z.to_i
-check = ""
-pos = []
+=end
 
-for i in 0...b
-  check = arr[i]
-  cnt = 0
-  s = 0
+
+class DeletingOccurrences
+  def del(arr,occ)
+    b = arr.length
+    pos = []
+    check = ""
+    for i in 0...b
+      check = arr[i]
+      cnt = 0
+      s = 0
   
-  for j in 0...b
-    if check == arr [j]
-      pos[cnt] = j
-      cnt += 1
-    end
-  end
+      for j in 0...b
+        if check == arr [j]
+          pos[cnt] = j
+          cnt += 1
+        end
+      end
 
-  while cnt > occ
-    arr.delete_at(pos[cnt - 1])
-    cnt -= 1
+      while cnt > occ
+        arr.delete_at(pos[cnt - 1])
+        cnt -= 1
+      end
+    end
+    puts"#{arr}" 
+    return arr
   end
 end
-puts"#{arr}" 
