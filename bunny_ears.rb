6280999c5a2ed9524ebf366bedc7a955c0d286a7
Roshmk <roshanmk94@gmail.com>
2021-04-22 08:40:27 +0530

    Made it recursive

--- bunny_ears.rb
+++ bunny_ears.rb
@@ -1,8 +1,9 @@
 class BunnyEars
-  
+
   def cnt(a)
-    a *= 2
-    return a
+    return a if a == 0
+    e = 2 + cnt(a -= 1)
+    return e
   end
   
 end
@@ -11,14 +12,14 @@ require 'minitest/autorun'
 
 class Test_BunnyEars < Minitest::Test
     
-    def setup
-        @be = BunnyEars.new
-    end
+  def setup
+    @be = BunnyEars.new
+  end
 
-    def test_strike_1
-        assert_equal 8, @be.cnt(4)
-        assert_equal 22, @be.cnt(11)
-        assert_equal 0, @be.cnt(0)
-        assert_equal 102, @be.cnt(51)
-    end    
+  def test_strike_1
+    assert_equal 8, @be.cnt(4)
+    assert_equal 22, @be.cnt(11)
+    assert_equal 0, @be.cnt(0)
+    assert_equal 102, @be.cnt(51)
+  end    
 end
